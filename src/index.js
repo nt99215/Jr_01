a08520c77a591c5c5ae43394585f5a44fb4f5195
nt99258 <nt99258@nts-corp.com>
2020-05-26 10:19:00 +0900

    boot modify

--- src/index.js
+++ src/index.js
@@ -41,5 +41,47 @@ export default class index extends Phaser.Game {
 
 }
 
+function resize () {
+    if(window.nts.index) {
+        const ww = win.innerWidth;
+        const hh = win.innerHeight;
+
+        // var debugs:any = document.getElementById("debugs");
+        // debugs.value += "\n"+"w : "+ww + " H : " +hh;
+
+        if(win.nts.winSize && win.nts.winSize.ww ==ww && win.nts.winSize.hh == hh) return;
+        win.nts.winSize = {ww:ww, hh:hh};
+
+        let w = 1280;
+        let h = 720;
+        const scale = Math.min(ww/w, hh/h);
+        w = Math.round(w * scale);
+        h = Math.round(h * scale);
+        const marginTop = (hh - h) >> 1;
+        const marginLeft = (ww - w) >> 1;
+
+        window.nts.index.canvas.setAttribute('style',
+            `display:block; -ms-transform: scale(${scale}); 
+        -webkit-transform: scale3d(${scale}, 1);
+         -moz-transform: scale(${scale}); 
+         -o-transform: scale(${scale}); 
+         transform: scale(${scale});
+         transform-origin: top left;
+         margin-left: ${marginLeft}px; margin-top: ${marginTop}px;
+         `
+        );
+        window.nts.index.scale.setGameSize(1280,720);
+    }
+}
+
+const win = window;
 window.nts = {};
 window.nts.index = new index('main_doc', 1280, 720);
+
+if(window.nts.index.isBooted ) {
+    resize ();
+}
+
+window.nts.index.events.once('boot', ()=> {
+    resize();
+});
